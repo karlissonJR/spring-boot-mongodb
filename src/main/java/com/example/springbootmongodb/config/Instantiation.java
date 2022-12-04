a4fc044ff3f390b3562af7621c3a0a91695d75ea
karlissonJR <karlissonj@gmail.com>
2022-12-04 10:31:51 -0300

    add comentarios nos posts

--- src/main/java/com/example/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/example/springbootmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.example.springbootmongodb.config;
 import com.example.springbootmongodb.domain.Post;
 import com.example.springbootmongodb.domain.User;
 import com.example.springbootmongodb.dto.AuthorDTO;
+import com.example.springbootmongodb.dto.CommentDTO;
 import com.example.springbootmongodb.repositories.PostRepository;
 import com.example.springbootmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, LocalDateTime.now(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO comment1 = new CommentDTO("Boa viagem mano!", LocalDateTime.now(), new AuthorDTO(alex));
+        CommentDTO comment2 = new CommentDTO("Aproveite", LocalDateTime.now(), new AuthorDTO(bob));
+        CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", LocalDateTime.now(), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(comment1, comment2));
+        post2.getComments().add(comment3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/example/springbootmongodb/domain/Post.java
+++ src/main/java/com/example/springbootmongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package com.example.springbootmongodb.domain;
 
 import com.example.springbootmongodb.dto.AuthorDTO;
+import com.example.springbootmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -19,6 +22,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {}
 
@@ -68,6 +72,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
