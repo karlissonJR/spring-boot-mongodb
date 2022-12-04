2b1532bc61a8558eacb3d3194c73728909302c8d
karlissonJR <karlissonj@gmail.com>
2022-12-04 08:39:30 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/example/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/example/springbootmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.example.springbootmongodb.config;
 
 import com.example.springbootmongodb.domain.Post;
 import com.example.springbootmongodb.domain.User;
+import com.example.springbootmongodb.dto.AuthorDTO;
 import com.example.springbootmongodb.repositories.PostRepository;
 import com.example.springbootmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.*;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 @Configuration
@@ -31,10 +31,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, LocalDateTime.now(), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, LocalDateTime.now(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
